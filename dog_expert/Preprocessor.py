3251875fa8556e1f8323c112ab607f14e8498cb5
wprazuch <wojciechprazuch1@gmail.com>
2020-10-29 14:40:42 +0100

    Preparing to create a pipeline for dataset, and data preprocessing and loading

---
+++ dog_expert/Preprocessor.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+
+class Preprocessor:
+    
+    def __init__(self):
---
+++ dog_expert/utils.py
@@ -0,0 +1,50 @@
+import os
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import pathlib
+
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.image import resize
+
+def load_by_classes(input_path, no_images=None):
+    data = []
+    labels = []
+    for class_dir in tqdm(os.listdir(input_path)):
+        path = os.path.join(input_path, class_dir)
+        if no_images is None:
+            files = os.listdir(path)
+        else:
+            files = os.listdir(path)[:no_images]
+        for file in files:
+            img = load_img(pathlib.Path(path, file))
+            img = np.array(img)
+            data.append(img)
+            labels.append(class_dir)
+    return data, labels
+
+
+def preprocess_input(data, labels):
+    for i in tqdm(range(len(data))):
+        try:
+            data[i] = data[i].astype(np.float32)
+            data[i] = resize(data[i], IMAGE_SIZE).numpy()
+            #data[i] = rgb2gray(data[i])
+            data[i] = 1./255. * data[i]
+        except:
+            del data[i]
+            del labels[i]
+    return data, labels
+
+
+def load_data(path, labels_df=None):
+    data = []
+    labels = []
+    for file in tqdm(os.listdir(path)):
+        img = load_img(pathlib.Path(path, file))
+        img = np.array(img)
+        data.append(img)
+        if labels_df is not None:
+            target = labels_df.loc[labels_df['id'].str.startswith(file.split('.')[0]), 'target'].values[0]
+            labels.append(target)
+    return data, labels
